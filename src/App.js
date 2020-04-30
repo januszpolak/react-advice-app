f01bbe8bef2f6e5b445ee3eea477fa6025e8b531
januszpolak <janusz.polak@interia.pl>
2020-04-30 17:22:46 +0200

    initial app version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.newAdvice();
+  }
+
+  newAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        const advice = data.slip.advice;
+        this.setState({ advice });
+        // console.log(data.slip.advice);
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>{this.state.advice}</h1>
+        <button onClick={this.newAdvice}>Click me!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
